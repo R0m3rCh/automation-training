124f60b49ce919bbdb7e2473b4158f03e271c411
Romer Chavez Patzi <Romer Chavez@jalasoft.local>
2020-02-10 08:59:20 -0400

    corrections in tests

--- calculator/spec/calculatorSpec.js
+++ calculator/spec/calculatorSpec.js
@@ -1,22 +1,22 @@
 var calculator = require("../calculator.js")
 
 describe("Operations", function() {
-  it("The function should add 2 numbers",function() {
+  it("returns the addition of 2 numbers",function() {
     var value = calculator.addition(3, 7)
     expect(value).toBe(10)
   })
 
-  it("The function should rest 2 numbers",function() {
+  it("returns the substraction of 2 numbers",function() {
     var value = calculator.subtraction(8, 2)
     expect(value).toBe(6)
   })
 
-  it("The function should return the product of 2 numbers",function() {
+  it("returns the product of 2 numbers",function() {
     var value = calculator.multiplication(8, 2)
     expect(value).toBe(16)
   })
 
-  it("The function should return the divition of 2 numbers",function() {
+  it("returns the divition of 2 numbers",function() {
     var value = calculator.division(8, 2)
     expect(value).toBe(4)
   })
--- calculator/spec/validatorSpec.js
+++ calculator/spec/validatorSpec.js
@@ -1,12 +1,12 @@
 var validator = require("../validator.js")
 
 describe("Validations", function() {
-    it("The function should return false if values are negative",function() {
+    it("returns false if values are negative",function() {
       var value = validator.validate(-3, -7)
       expect(value).toBe(false)
     })
 
-    it("The function should return false if numbers are negative",function() {
+    it("returns false if numbers are negative",function() {
         var value = validator.validate('3', '7')
         expect(value).toBe(false)
       })
