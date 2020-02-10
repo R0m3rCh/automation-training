9cd5c53855d68459d4b51e24e6ee94788c138863
Romer Chavez Patzi <Romer Chavez@jalasoft.local>
2020-02-10 09:31:29 -0400

    test description

--- calculator/spec/calculatorSpec.js
+++ calculator/spec/calculatorSpec.js
@@ -1,22 +1,22 @@
 var calculator = require("../calculator.js")
 
 describe("Operations", function() {
-  it("returns the addition of 2 numbers",function() {
+  it("Returns the addition of 2 numbers",function() {
     var value = calculator.addition(3, 7)
     expect(value).toBe(10)
   })
 
-  it("returns the substraction of 2 numbers",function() {
+  it("Returns the substraction of 2 numbers",function() {
     var value = calculator.subtraction(8, 2)
     expect(value).toBe(6)
   })
 
-  it("returns the product of 2 numbers",function() {
+  it("Returns the product of 2 numbers",function() {
     var value = calculator.multiplication(8, 2)
     expect(value).toBe(16)
   })
 
-  it("returns the divition of 2 numbers",function() {
+  it("Returns the divition of 2 numbers",function() {
     var value = calculator.division(8, 2)
     expect(value).toBe(4)
   })
